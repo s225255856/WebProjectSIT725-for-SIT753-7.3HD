296c0288798f1a28a0fca8d00d3b40f8b2d0be3a
Teddy <teddyhiny@gmail.com>
2025-05-07 16:24:25 +1000

    refine ejs folder structure and add secret angel folder

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -3,21 +3,23 @@ const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
 const jwt = require('jsonwebtoken');
 
+
+
 router.get('/', authMiddleware, (req, res) => {
   const user = req?.user || null;
   res.render('home', { title: 'Home Page', error: null, user });
 });
 
 router.get('/login', (req, res) => {
-  res.render('login', { error: null, user: null });
+  res.render('auth/login', { error: null, user: null });
 });
 
 router.get('/signup', (req, res) => {
-  res.render('signup', { error: null, user: null });
+  res.render('auth/signup', { error: null, user: null });
 });
 
 router.get('/settings', authMiddleware, (req, res) => {
-  res.render('userSetting', { error: null, user: req.user });
+  res.render('auth/userSetting', { error: null, user: req.user });
 });
 
 router.get('/reset-password/:token', (req, res) => {
@@ -36,12 +38,16 @@ router.get('/reset-password/:token', (req, res) => {
 
     const user = decoded.user;
 
-    res.render('reset-password', { user, token });
+    res.render('auth/reset-password', { user, token });
   } catch (err) {
 
     console.error('Error verifying token:', err);
     res.status(400).send('Invalid or expired reset link');
   }
+});
+
+router.get('/secret-angel', authMiddleware, (req, res) => {
+  res.render('secretAngel/index', { error: null, user: req.user });
 });
 
 module.exports = router;
