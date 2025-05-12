a535d3bff59c2e1c3b42760035ec1c597b8b7ddc
Teddy <teddyhiny@gmail.com>
2025-05-12 12:44:13 +1000

    add modal for secret angel

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,14 @@ const usersRouter = require('./userRoutes');
 
 const pageRouter = require('./pageRoutes');
 
+const secretAngelRouter = require('./secretAngelRoutes');
+
 router.use('/', pageRouter);
 
 router.use('/api/users', usersRouter);
+
+router.use('/api/secretAngel', secretAngelRouter);
+
+
 
 module.exports = router;
